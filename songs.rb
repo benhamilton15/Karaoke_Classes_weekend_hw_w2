0802c1691082095de4c74a2c1429b21f95b919cc
Ben Hamilton <ben.hamilton@me.com>
2019-04-12 14:31:29 +0100

    created files for songs, guests and rooms. Both classes and tests

---
+++ songs.rb
@@ -0,0 +1,9 @@
+class Songs
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
---
+++ specs/songs_spec.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../songs.rb')
+
+class SongsTest < MiniTest::Test
+
+  def setup
+    @song1("Gucci Gang")
+  end
+
+  def test_song_has_name
+    assert_equal("Gucci Gang", @song1.name)
+  end
+
+
+end
